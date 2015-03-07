3fdb0c5be2d782ac08cc9e12706376354f3ac8bc
Mike Lothar <mikelothar@gmail.com>
2015-03-08 00:40:14 +0100

    always leave the bg layer visible after saving

--- functions/save-as-png.jsx
+++ functions/save-as-png.jsx
@@ -48,47 +48,7 @@ function saveAsPng(dir, file) {
 
   }
 
-
-  /*
-
-  // create large graphs first, to make the path smooth
-  activeDocument.resizeImage(1500, 1100);
-  turnPathToRaster(lineName, pathItem);
-
-  app.activeDocument.artLayers.getByName('expReturnCurveMax').visible = false;
-  app.activeDocument.artLayers.getByName('expReturnCurveMin').visible = false;
-  app.activeDocument.artLayers.getByName('expReturnCurveMaxLarge').visible = true;
-  app.activeDocument.artLayers.getByName('expReturnCurveMinLarge').visible = true;
-
-  app.activeDocument.artLayers.getByName('expReturnCurveLarge').visible = true;
-  app.activeDocument.artLayers.getByName('expReturnCurve').visible = false;
+  // always leave the background layer visible after saving
   app.activeDocument.artLayers.getByName('background').visible = true;
-  save(file, 'large');
-  app.activeDocument.artLayers.getByName('background').visible = false;
-  save(file, 'nobg-large');
-
-  // now the small graphs
-  activeDocument.resizeImage(450, 330);
-
-  app.activeDocument.artLayers.getByName('expReturnCurveMax').visible = true;
-  app.activeDocument.artLayers.getByName('expReturnCurveMin').visible = true;
-  app.activeDocument.artLayers.getByName('expReturnCurveMaxLarge').visible = false;
-  app.activeDocument.artLayers.getByName('expReturnCurveMinLarge').visible = false;
-
-  app.activeDocument.artLayers.getByName('expReturnCurveLarge').visible = false;
-  app.activeDocument.artLayers.getByName('expReturnCurve').visible = true;
-  save(file, 'nobg');
-  app.activeDocument.artLayers.getByName('background').visible = true;
-  save(file);
-
-  function save(fileName, ext1) {
-    if (ext1) fileName += '-' + ext1;
-    fileName += '.png';
-    var saveFile = File(dir + '/' + fileName);
-    if (saveFile.exists) saveFile.remove();
-    activeDocument.saveAs(saveFile, pngOpts, true, Extension.LOWERCASE);
-  }
-
-  */
 
 }
