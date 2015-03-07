c1fbe88357a32d1f0b1ab04960249a96a8f0b8fa
Mike Lothar <mikelothar@gmail.com>
2015-03-08 00:36:42 +0100

    added comment

--- setup-and-run.jsx
+++ setup-and-run.jsx
@@ -1,7 +1,7 @@
 
 // Where to put the graphs
 //var destinationFolder = "C:/graphs/2015/v4/delete";
-var destinationFolder = "/Users/MLP/Repos/temp2";
+var destinationFolder = "/Users/MLP/Repos/graphs";
 
 // Data
 #include 'values/2015/private-banking/se/historical-development.jsx';
@@ -9,18 +9,12 @@ var destinationFolder = "/Users/MLP/Repos/temp2";
 #include 'values/2015/private-banking/se/min-exp-max-returns.jsx';
 #include 'values/2015/retail-banking/se/min-exp-max-returns.jsx';
 
-// Choose what to run
+// Choose what to run (the PSD that is open in Photoshop)
 //#include 'scripts/HistoricalDevelopment.jsx';
 //#include 'scripts/ReturnLine20.jsx';
 //#include 'scripts/ReturnLine.jsx';
 //#include 'scripts/ReturnTable.jsx';
 #include 'scripts/ReturnTable20.jsx';
-
-
-
-
-
-
 
 // Needed functions (leave as is)
 #include 'functions/functions.jsx';
